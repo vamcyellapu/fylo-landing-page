d257853da9613a28937fcbae81a3a4b4e4235551
vamsi <vamcyellapu@gmail.com>
2022-12-10 08:05:27 +0530

    build the input group component

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+const btn = document.querySelector(".btn");
+const input = document.querySelector(".input");
+const errormsg = document.querySelector(".input__error");
+const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+console.log(input.value);
+function keyPress() {
+  if (!input.value.match(pattern)) {
+    errormsg.classList.add("display");
+    input.classList.add("display");
+  } else {
+    errormsg.classList.remove("display");
+    input.classList.remove("display");
+  }
+}
+btn.addEventListener("click", keyPress);
